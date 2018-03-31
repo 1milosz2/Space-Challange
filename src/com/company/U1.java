31af34bdc402aa528caf3602fd576b9008c49952
1milosz2 <1milosz2@gmail.com>
2018-03-31 23:59:13 +0200

    .

---
+++ src/com/company/U1.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Random;
+
+public class U1 extends Rocket {
+    private int cargoLimit;
+    private int launchFailChancePercentage;
+    private int landFailChancePercentage;
+
+
+    public U1(int cost, int weightEmpty, int maxWeight, int launchFailChancePercentage, int landFailChancePercentage) {
+        super(cost, weightEmpty, maxWeight);
+        this.cargoLimit = maxWeight - weightEmpty;
+        this.launchFailChancePercentage = launchFailChancePercentage;
+        this.landFailChancePercentage = landFailChancePercentage;
+    }
+
+    @Override
+    public boolean land() {
+        int random = new Random().nextInt(100);
+        if (random < landFailChancePercentage * ((getWeightCurrent() - getWeightEmpty()) / cargoLimit)){
+            return false;
+        }
+        else return true;
+    }
+
+    @Override
+    public boolean launch() {
+        int random = new Random().nextInt(100);
+        if (random < launchFailChancePercentage * ((getWeightCurrent() - getWeightEmpty()) / cargoLimit)){
+            return false;
+        }
+        else return true;
+    }
+}
